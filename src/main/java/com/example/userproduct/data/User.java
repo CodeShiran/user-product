e8fa7396cf95c7e4812848ca1c8479a40eb21cf2
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-06-10 08:43:09 +0530

    user entity created

---
+++ src/main/java/com/example/userproduct/data/User.java
@@ -0,0 +1,24 @@
+package com.example.userproduct.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
